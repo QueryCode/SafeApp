97fcf04cc30e6772ef1cc8fe63e6c485da534f5d
QueryCode <1172894032@qq.com>
2015-07-07 20:18:51 +0800

    获取手机上所有app的信息方法

---
+++ Safe/src/com/donnie/safe/service/AppInfoService.java
@@ -0,0 +1,58 @@
+package com.donnie.safe.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.drawable.Drawable;
+
+import com.donnie.safe.bean.AppInfo;
+
+/**  
+ * @Title: AppInfoService.java
+ * @Package com.donnie.safe.service
+ * @Description: TODO(获取手机上所有app的信息)
+ * @author donnieSky
+ * @date 2015年7月7日 下午4:39:05   
+ * @version V1.0  
+ */
+public class AppInfoService {
+	
+	private Context context;
+	private PackageManager manager;
+	
+	public AppInfoService(Context context) {
+		// TODO Auto-generated constructor stub
+		this.context = context;
+		manager = context.getPackageManager();
+	}
+	
+	//获取所有应用程序信息
+	public List<AppInfo> getAppInfos(){
+		List<AppInfo> appInfos = new ArrayList<AppInfo>();
+		List<ApplicationInfo> applicationInfos = manager.getInstalledApplications(PackageManager.GET_UNINSTALLED_PACKAGES);
+		for (ApplicationInfo appInfo : applicationInfos) {
+			AppInfo info = new AppInfo();
+			Drawable app_icon = appInfo.loadIcon(manager);
+			String app_name = appInfo.loadLabel(manager).toString();
+			info.setApp_name(app_name);
+			info.setApp_icon(app_icon);
+			try {
+				PackageInfo packageInfo = manager.getPackageInfo(appInfo.packageName, 0);
+				String app_version = packageInfo.versionName;
+				info.setApp_version(app_version);
+				info.setPackagename(appInfo.packageName);
+			} catch (NameNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			appInfos.add(info);
+		}
+		return appInfos;
+	}
+
+}
