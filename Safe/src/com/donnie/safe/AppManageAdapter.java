e153bb7462bd93e9ca20a170b9b5a683cc866137
QueryCode <1172894032@qq.com>
2015-07-08 22:24:54 +0800

    实现popupwindow等功能

--- Safe/src/com/donnie/safe/AppManageAdapter.java
+++ Safe/src/com/donnie/safe/AppManageAdapter.java
@@ -5,14 +5,17 @@ import java.util.List;
 import com.donnie.safe.bean.AppInfo;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**  
  * @Title: AppManageAdapter.java
  * @Package com.donnie.safe
- * @Description: TODO(添加描述)
+ * @Description: TODO(程序列表显示adapter)
  * @author donnieSky
  * @date 2015年7月7日 下午5:29:26   
  * @version V1.0  
@@ -21,11 +24,17 @@ public class AppManageAdapter extends BaseAdapter{
 
 	private Context context;
 	private List<AppInfo> appInfos;
+	private LayoutInflater mInflater;
 	
+	public void setAppInfos(List<AppInfo> appInfos) {
+		this.appInfos = appInfos;
+	}
+
 	public AppManageAdapter(Context context,List<AppInfo> appInfos) {
 		// TODO Auto-generated constructor stub
 		this.context = context;
 		this.appInfos = appInfos;
+		mInflater = LayoutInflater.from(context);
 	}
 	
 	@Override
@@ -49,7 +58,22 @@ public class AppManageAdapter extends BaseAdapter{
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		return null;
+		View view = null;
+		if (convertView!=null) {
+			view = convertView;
+		}else {
+			view = mInflater.inflate(R.layout.app_item, null);
+		}
+		
+		ImageView appicon = (ImageView) view.findViewById(R.id.appicon);
+		TextView appname = (TextView) view.findViewById(R.id.appname);
+		TextView appversion = (TextView) view.findViewById(R.id.appversion);
+		
+		AppInfo appInfo = appInfos.get(position);
+		appicon.setImageDrawable(appInfo.getApp_icon());
+		appname.setText(appInfo.getApp_name());
+		appversion.setText(appInfo.getApp_version());
+		return view;
 	}
 
 }
